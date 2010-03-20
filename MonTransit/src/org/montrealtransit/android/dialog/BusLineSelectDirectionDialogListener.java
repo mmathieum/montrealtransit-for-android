0b0269b7c5664440e85b5ae1c599a34f5625debb
Mathieu Méa <mmathieum@users.noreply.github.com>
2010-03-20 22:20:35 +0000

    Move dialog from the activity package to a new dialog package. Old dialog are deprecated for now. Will be removed soon.

---
+++ MonTransit/src/org/montrealtransit/android/dialog/BusLineSelectDirectionDialogListener.java
@@ -0,0 +1,16 @@
+package org.montrealtransit.android.dialog;
+
+/**
+ * This interface must be implemented by all the classes that want to be able to manage the BusSelectDirection dialog.
+ * @author Mathieu Méa
+ */
+public interface BusLineSelectDirectionDialogListener {
+	
+	/**
+	 * Show the selected bus line (direction).
+	 * @param lineNumber the line number
+	 * @param directionId the line direction
+	 */
+	 void showNewLine(String lineNumber, String directionId);
+
+}
