039f41a7f426d979cd95992f1618daeb485dc912
mathieu.mea <mathieu.mea@70aa8380-c310-11de-8ec6-755e00613368>
2010-03-21 01:28:23 +0000

    Add Radar(Compass) option to localize a subway station or a bus stop. Use Geocoder in a asynctask to get the GPS location of a bus stop.

---
+++ trunk/MonTransit/src/org/montrealtransit/android/service/ReverseGeocodeTaskListener.java
@@ -0,0 +1,18 @@
+package org.montrealtransit.android.service;
+
+import java.util.List;
+
+import android.location.Address;
+
+/**
+ * Interface implemented by all the class that need to process the result of {@link ReverseGeocodeTask}.
+ * @author Mathieu Méa
+ */
+public interface ReverseGeocodeTaskListener {
+
+	/**
+	 * Call when the task is complete to process the data.
+	 * @param addresses the new addresses.
+	 */
+	void processLocation(List<Address> addresses);
+}
