21617d49bc183c64b9b0a0148858b9513a1468e4
Mathieu Méa <mmathieum@users.noreply.github.com>
2010-11-01 23:06:41 +0000

    add links to subway service about dialog add custom HTTP 500 error message for subway service RSS twitter feed fix encoding error in subway service RSS twitter feed

--- MonTransit/src/org/montrealtransit/android/Constant.java
+++ MonTransit/src/org/montrealtransit/android/Constant.java
@@ -48,6 +48,7 @@ public class Constant {
 	public static final String HTML_CODE_EACUTE_2 = "&#233;";
 	public static final String HTML_CODE_ECIRC = "&ecirc;";
 	public static final String HTML_CODE_ECIRC_2 = "&#234;";
+	public static final String HTML_CODE_OCIRC = "&#244;";
 	public static final String HTML_TABLE_END = "</table>";
 	public static final String HTML_TAG = "<html>";
 	public static final String HTML_TAG_END = "</html>";
