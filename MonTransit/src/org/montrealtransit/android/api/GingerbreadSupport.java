ed16a871d6c967fad313728284ca4b3297404fe9
Mathieu Méa <mmathieum@users.noreply.github.com>
2010-12-13 23:27:31 +0000

    add API Level compatibility support classes

---
+++ MonTransit/src/org/montrealtransit/android/api/GingerbreadSupport.java
@@ -0,0 +1,16 @@
+package org.montrealtransit.android.api;
+
+import android.content.Context;
+import android.content.SharedPreferences.Editor;
+
+public class GingerbreadSupport extends FroyoSupport {
+	
+	public GingerbreadSupport(Context context) {
+		super(context);
+	}
+	
+	@Override
+	public void applySharedPreferencesEditor(Editor editor) {
+	    editor.apply();
+	}
+}
---
+++ MonTransit/src/org/montrealtransit/android/api/SupportUtil.java
@@ -0,0 +1,10 @@
+package org.montrealtransit.android.api;
+
+import android.content.SharedPreferences.Editor;
+
+
+public interface SupportUtil {
+
+	void applySharedPreferencesEditor(Editor editor);
+
+}
