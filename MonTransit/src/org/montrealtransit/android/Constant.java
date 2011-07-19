851f002a01eea31b6a0315579eae5e1ee0d881ca
Mathieu Méa <mmathieum@users.noreply.github.com>
2011-07-19 22:41:05 +0000

    improve bus stop place cleaning (again)

--- MonTransit/src/org/montrealtransit/android/Constant.java
+++ MonTransit/src/org/montrealtransit/android/Constant.java
@@ -18,14 +18,17 @@ public class Constant {
 	public static final String PLACE_CHAR_DE = "de ";
 	public static final int PLACE_CHAR_DE_LENGTH = PLACE_CHAR_DE.length();
 	
-	public static final String PLACE_CHAR_DE_LA = "de la ";
-	public static final int PLACE_CHAR_DE_LA_LENGTH = PLACE_CHAR_DE_LA.length();
+//	public static final String PLACE_CHAR_DE_LA = "de la ";
+//	public static final int PLACE_CHAR_DE_LA_LENGTH = PLACE_CHAR_DE_LA.length();
 
 	public static final String PLACE_CHAR_DES = "des ";
 	public static final int PLACE_CHAR_DES_LENGTH = PLACE_CHAR_DES.length();
 
 	public static final String PLACE_CHAR_DU = "du ";
 	public static final int PLACE_CHAR_DU_LENGTH = PLACE_CHAR_DU.length();
+	
+	public static final String PLACE_CHAR_LA = "la ";
+	public static final int PLACE_CHAR_LA_LENGTH = PLACE_CHAR_LA.length();
 	
 	public static final String PLACE_CHAR_L = "l'";
 	public static final int PLACE_CHAR_L_LENGTH = PLACE_CHAR_L.length();
@@ -34,12 +37,14 @@ public class Constant {
 
 	public static final String PLACE_CHAR_IN_DE = PLACE_CHAR_IN + PLACE_CHAR_DE;
 
-	public static final String PLACE_CHAR_IN_DE_LA = PLACE_CHAR_IN + PLACE_CHAR_DE_LA;
+	//public static final String PLACE_CHAR_IN_DE_LA = PLACE_CHAR_IN + PLACE_CHAR_DE_LA;
 
 	public static final String PLACE_CHAR_IN_DES = PLACE_CHAR_IN + PLACE_CHAR_DES;
 
 	public static final String PLACE_CHAR_IN_DU = PLACE_CHAR_IN + PLACE_CHAR_DU;
 
+	public static final String PLACE_CHAR_IN_LA = PLACE_CHAR_IN + PLACE_CHAR_LA;
+	
 	public static final String PLACE_CHAR_IN_L = PLACE_CHAR_IN + PLACE_CHAR_L;
 
 	public static final String PLACE_CHAR_PARENTHESE = "(";
