99524e4bf05dbf088a623cffdc1fff4c9a662094
mathieu.mea <mathieu.mea@70aa8380-c310-11de-8ec6-755e00613368>
2011-10-23 11:44:07 +0000

    Add basic support for Ice Cream Sandwich

---
+++ trunk/MonTransit/src/org/montrealtransit/android/api/IceCreamSandwichSupport.java
@@ -0,0 +1,10 @@
+package org.montrealtransit.android.api;
+
+import android.content.Context;
+
+public class IceCreamSandwichSupport extends HoneycombSupport {
+
+	public IceCreamSandwichSupport(Context context) {
+		super(context);
+	}
+}
