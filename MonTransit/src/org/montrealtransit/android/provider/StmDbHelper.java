0c2ea64e60f236e0285cea14987fccffd5cb9b34
Mathieu Méa <mmathieum@users.noreply.github.com>
2012-09-21 05:15:56 +0000

    bump DB version to 17

--- MonTransit/src/org/montrealtransit/android/provider/StmDbHelper.java
+++ MonTransit/src/org/montrealtransit/android/provider/StmDbHelper.java
@@ -36,7 +36,7 @@ public class StmDbHelper extends SQLiteOpenHelper {
 	/**
 	 * The database version use to manage database changes.
 	 */
-	public static final int DB_VERSION = 16;
+	public static final int DB_VERSION = 17;
 
 	/**
 	 * The list of SQL dump files.
