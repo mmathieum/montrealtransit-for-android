9acf7c8503222d354ac89cc781449bcdde532bbd
mathieu.mea@gmail.com <mathieu.mea@gmail.com@70aa8380-c310-11de-8ec6-755e00613368>
2013-06-10 22:56:37 +0000

    Improve bike station UI/Refresh.

--- trunk/MonTransit/src/org/montrealtransit/android/Constant.java
+++ trunk/MonTransit/src/org/montrealtransit/android/Constant.java
@@ -90,7 +90,6 @@ public class Constant {
 	 * The shortcut for maps.
 	 */
 	public static final char SHORTCUT_MAPS = 'm';
-
 	/**
 	 * The number of feet necessary to make 1 meter.
 	 */
@@ -103,4 +102,8 @@ public class Constant {
 	 * The number of meter necessary to make 1 KM.
 	 */
 	public static float METER_PER_KM = 1000f;
+	/**
+	 * Vertical offset for top toast.
+	 */
+	public static final int TOAST_Y_OFFSET = 200;
 }
