c2ad2814e3963c06e500dd1d10d0e8eeb8e53960
Mathieu Méa <mmathieum@users.noreply.github.com>
2013-11-05 23:40:22 +0000

    Add ping URI to wake up the content provider (remove app icon...)

--- MonTransit/src/org/montrealtransit/android/provider/StmBusScheduleManager.java
+++ MonTransit/src/org/montrealtransit/android/provider/StmBusScheduleManager.java
@@ -10,6 +10,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.text.TextUtils;
 
 public class StmBusScheduleManager {
@@ -19,16 +20,57 @@ public class StmBusScheduleManager {
 	private static final String[] PROJECTION_SCHEDULE = new String[] { "departure" };
 
 	public static final String AUTHORITY = "org.montrealtransit.android.schedule.stmbus";
-	
+
 	public static final String PROVIDER_APP_PACKAGE = "org.montrealtransit.android.schedule.stmbus";
 
 	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/");
+
+	public static final String PING_CONTENT_DIRECTORY = "ping";
 
 	public static final String ROUTE_CONTENT_DIRECTORY = "route"; // mandatory
 	public static final String TRIP_CONTENT_DIRECTORY = "trip";
 	public static final String STOP_CONTENT_DIRECTORY = "stop";
 	public static final String DATE_CONTENT_DIRECTORY = "date";
 	public static final String TIME_CONTENT_DIRECTORY = "time";
+
+	private static boolean pingChecked = false;
+
+	public static void wakeUp(final ContentResolver contentResolver) {
+		MyLog.v(TAG, "wakeUp()");
+		if (pingChecked) {
+			return;
+		}
+		new AsyncTask<Void, Void, Void>() {
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				// MyLog.v(TAG, "wakeUp() > doInBackground()");
+				ping(contentResolver);
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(Void result) {
+				// MyLog.v(TAG, "wakeUp() > onPostExecute()");
+				super.onPostExecute(result);
+			}
+		}.execute();
+		pingChecked = true;
+	}
+
+	public static void ping(final ContentResolver contentResolver) {
+		MyLog.v(TAG, "ping()");
+		Cursor cursor = null;
+		try {
+			cursor = contentResolver.query(Uri.withAppendedPath(CONTENT_URI, PING_CONTENT_DIRECTORY), null, null, null, null);
+		} catch (Throwable t) {
+			MyLog.w(TAG, t, "Error!");
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+	}
 
 	public static List<String> findBusScheduleList(ContentResolver contentResolver, String busLineNumber, String stopCode, String date, String time) {
 		MyLog.v(TAG, "findBusScheduleList(%s, %s, %s, %s)", busLineNumber, stopCode, date, time);
@@ -45,6 +87,8 @@ public class StmBusScheduleManager {
 					} while (cursor.moveToNext());
 				}
 			}
+		} catch (Throwable t) {
+			MyLog.w(TAG, t, "Error!");
 		} finally {
 			if (cursor != null)
 				cursor.close();
@@ -72,5 +116,4 @@ public class StmBusScheduleManager {
 	public static boolean isAppInstalled(Context context) {
 		return Utils.isPackageExists(context, PROVIDER_APP_PACKAGE);
 	}
-
 }
