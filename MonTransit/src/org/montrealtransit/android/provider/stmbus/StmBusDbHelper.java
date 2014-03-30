512bdd0efaf5cb1d539866e3513858d44cb12daf
Mathieu Méa <mmathieum@users.noreply.github.com>
2014-03-30 21:41:14 +0000

    bump STM Bus DB version to 6 bump STM Subway schedule DB version to 4

--- MonTransit/src/org/montrealtransit/android/provider/stmbus/StmBusDbHelper.java
+++ MonTransit/src/org/montrealtransit/android/provider/stmbus/StmBusDbHelper.java
@@ -13,7 +13,7 @@ public class StmBusDbHelper extends AbstractDbHelper {
 
 	public static final String DB_NAME = "stmbus.db";
 
-	public static final int DB_VERSION = 5; // 2014-03-07
+	public static final int DB_VERSION = 6; // 2014-03-30
 	
 	public static final int LABEL = R.string.ca_mtl_stm_bus_label;
 
