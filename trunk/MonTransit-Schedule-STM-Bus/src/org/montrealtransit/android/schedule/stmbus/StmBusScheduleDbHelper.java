c3df8cc57b2328771b033d64ea495f4f29918378
mathieu.mea@gmail.com <mathieu.mea@gmail.com@70aa8380-c310-11de-8ec6-755e00613368>
2014-03-30 22:08:57 +0000

    bump STM Bus schedule DB version to 6

--- trunk/MonTransit-Schedule-STM-Bus/src/org/montrealtransit/android/schedule/stmbus/StmBusScheduleDbHelper.java
+++ trunk/MonTransit-Schedule-STM-Bus/src/org/montrealtransit/android/schedule/stmbus/StmBusScheduleDbHelper.java
@@ -20,7 +20,7 @@ public class StmBusScheduleDbHelper extends SQLiteOpenHelper {
 
 	private static final String RAW_FILE_FORMAT = "ca_mtl_stm_bus_schedules_route_%s";
 
-	public static final int DB_VERSION = 5; // 2014-03-07
+	public static final int DB_VERSION = 6; // 2014-03-30
 
 	public static final String T_SCHEDULES = "schedules";
 	// public static final String T_SCHEDULES_K_ID = BaseColumns._ID;
