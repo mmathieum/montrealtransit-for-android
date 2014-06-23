75be748f25ab344a52d70190832508e5ccd69539
mathieu.mea@gmail.com <mathieu.mea@gmail.com@70aa8380-c310-11de-8ec6-755e00613368>
2014-06-23 01:01:05 +0000

    bump STM Bus DB version to 9 bump STM Subway DB version to 3 bump STM Subway schedule DB version to 8

--- trunk/MonTransit/src/org/montrealtransit/android/provider/stmbus/StmBusDbHelper.java
+++ trunk/MonTransit/src/org/montrealtransit/android/provider/stmbus/StmBusDbHelper.java
@@ -13,8 +13,8 @@ public class StmBusDbHelper extends AbstractDbHelper {
 
 	public static final String DB_NAME = "stmbus.db";
 
-	public static final int DB_VERSION = 8; // 2014-04-28
-	
+	public static final int DB_VERSION = 9; // 2014-06-23
+
 	public static final int LABEL = R.string.ca_mtl_stm_bus_label;
 
 	public StmBusDbHelper(Context context) {
@@ -32,21 +32,21 @@ public class StmBusDbHelper extends AbstractDbHelper {
 	public int getDbVersion() {
 		return DB_VERSION;
 	}
-	
-	@Override
-	public String getUID() {
-		return StmBusProvider.AUTHORITY; // TODO "ca_mtl_stm_bus";
-	}
+
+	// @Override
+	// public String getUID() {
+	// return StmBusProvider.AUTHORITY; // TODO "ca_mtl_stm_bus";
+	// }
 
 	@Override
 	public String getDbName() {
 		return DB_NAME;
 	}
-	
-	@Override
-	public int getLabel() {
-		return LABEL;
-	}
+
+	// @Override
+	// public int getLabel() {
+	// return LABEL;
+	// }
 
 	@Override
 	public int[] getRouteFiles() {
