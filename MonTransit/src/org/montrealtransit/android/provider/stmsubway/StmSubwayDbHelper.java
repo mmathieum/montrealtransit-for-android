49173d6441f2172b2b073c1e7883f49cd949cd20
Mathieu Méa <mmathieum@users.noreply.github.com>
2014-06-23 01:01:05 +0000

    bump STM Bus DB version to 9 bump STM Subway DB version to 3 bump STM Subway schedule DB version to 8

--- MonTransit/src/org/montrealtransit/android/provider/stmsubway/StmSubwayDbHelper.java
+++ MonTransit/src/org/montrealtransit/android/provider/stmsubway/StmSubwayDbHelper.java
@@ -13,7 +13,7 @@ public class StmSubwayDbHelper extends AbstractDbHelper {
 
 	public static final String DB_NAME = "stmsubway.db";
 
-	public static final int DB_VERSION = 2; // 2014-01-06
+	public static final int DB_VERSION = 3; // 2014-06-23
 
 	public static final int LABEL = R.string.ca_mtl_stm_subway_label;
 
@@ -33,20 +33,20 @@ public class StmSubwayDbHelper extends AbstractDbHelper {
 		return DB_VERSION;
 	}
 
-	@Override
-	public String getUID() {
-		return StmSubwayProvider.AUTHORITY; // TODO "ca_mtl_stm_subway";
-	}
+	// @Override
+	// public String getUID() {
+	// return StmSubwayProvider.AUTHORITY; // TODO "ca_mtl_stm_subway";
+	// }
 
 	@Override
 	public String getDbName() {
 		return DB_NAME;
 	}
 
-	@Override
-	public int getLabel() {
-		return LABEL;
-	}
+	// @Override
+	// public int getLabel() {
+	// return LABEL;
+	// }
 
 	@Override
 	public int[] getRouteFiles() {
