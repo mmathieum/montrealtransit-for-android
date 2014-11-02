c8465d61a8367b963221b3d5d7ae4b307536e9a9
mathieu.mea@gmail.com <mathieu.mea@gmail.com@70aa8380-c310-11de-8ec6-755e00613368>
2014-11-02 14:30:47 +0000

    bump STM Subway schedule DB version to 13

--- trunk/MonTransit/src/org/montrealtransit/android/provider/stmsubway/schedule/StmSubwayScheduleDbHelper.java
+++ trunk/MonTransit/src/org/montrealtransit/android/provider/stmsubway/schedule/StmSubwayScheduleDbHelper.java
@@ -15,7 +15,7 @@ public class StmSubwayScheduleDbHelper extends SQLiteOpenHelper {
 
 	public static final String DB_NAME = "stmsubwayschedule_service_dates.db";
 
-	public static final int DB_VERSION = 12; // 2014-09-29
+	public static final int DB_VERSION = 13; // 2014-11-03
 
 	public static final String T_SERVICE_DATES = "service_dates";
 	// public static final String T_SERVICE_DATES_K_ID = BaseColumns._ID;
