9d576264328bce4b7e01c8ab49f84626b285e3cd
Mathieu Méa <mathieu.mea@gmail.com>
2018-03-23 20:04:03 -0400

    Getting ready for March 26 STM bus update & bump DB version

--- MonTransit-Schedule-STM-Bus/src/org/montrealtransit/android/schedule/stmbus/StmBusScheduleDbHelper.java
+++ MonTransit-Schedule-STM-Bus/src/org/montrealtransit/android/schedule/stmbus/StmBusScheduleDbHelper.java
@@ -15,7 +15,7 @@ public class StmBusScheduleDbHelper extends SQLiteOpenHelper {
 
 	public static final String DB_NAME = "stmbusschedule_service_dates.db";
 
-	public static final int DB_VERSION = 38; // 2018-01-08
+	public static final int DB_VERSION = 39; // 2018-03-26
 
 	public static final String T_SERVICE_DATES = "service_dates";
 	// public static final String T_SERVICE_DATES_K_ID = BaseColumns._ID;
